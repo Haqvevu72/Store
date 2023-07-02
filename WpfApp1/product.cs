ea81ff320438ead6000aaa989ac090b1ccab1985
Haqve_vu72 <Haqve_vu72@itstep.edu.az>
2023-07-02 06:14:36 +0400

    Some changes

---
+++ WpfApp1/product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    public class product
+    {
+        public string ImagePath { get; set; }
+        public string Name { get; set; }
+        public double Cost { get; set; }
+
+        public product(string path,string name, double cost)
+        {
+            ImagePath = path;
+            Name = name;
+            Cost = cost;
+        }
+    }
+}
